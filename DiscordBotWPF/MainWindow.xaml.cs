87ce7a55ba69dc5518e191398eb57b4c273532e7
Ken Hall <eviltek2099@gmail.com>
2021-11-06 13:40:36 -0400

    Works but missing responce roles still

--- DiscordBotWPF/MainWindow.xaml.cs
+++ DiscordBotWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Interactivity.Extensions;
 
 namespace DiscordBotWPF
 {
@@ -20,9 +28,102 @@ namespace DiscordBotWPF
   /// </summary>
   public partial class MainWindow : Window
   {
+    Server svr = new Server();
     public MainWindow()
     {
       InitializeComponent();
+      DataContext = svr;
+      svr.Start(new[] {
+        "https://worldmatrix.xyz/discordapi/"
+      });
+
+
+      MainAsync().GetAwaiter().GetResult();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      string DiscordUri = "https://discord.com/api/oauth2/authorize";
+      string Client_id = "905885667026944040";
+      string URL = DiscordUri + "?client_id=" + Client_id +
+        "&redirect_uri=https%3A%2F%2Fworldmatrix.xyz%2Fdiscordapi%2F&response_type=code&scope=identify";
+
+      Process.Start(URL);
+    }
+
+    public async Task MainAsync()
+    {
+      var Client = new DiscordClient(new DiscordConfiguration()
+      {
+        Token = "OTA1ODg1NjY3MDI2OTQ0MDQw.YYQlxQ.HRKtit66aB_LGwQwjcKNMcdn4MQ",
+        TokenType = TokenType.Bot,
+        AutoReconnect = true
+      });
+
+      Client.Ready += Client_Ready;
+      Client.GuildAvailable += Client_GuildAvailable;
+      Client.ClientErrored += Client_ClientError;
+
+      Client.MessageCreated += Handlers.MessageCreated;
+      Client.GuildMemberAdded += Handlers.MemberAdded;
+      Client.MessageUpdated += Handlers.MessageUpdated;
+      Client.ComponentInteractionCreated += Handlers.ComponentInteraction;
+
+      var commands = Client.UseCommandsNext(new CommandsNextConfiguration
+      {
+        StringPrefixes = new[] { "!" },
+        EnableMentionPrefix = false,
+        EnableDms = true,
+        EnableDefaultHelp = true,
+        DmHelp = true,
+        CaseSensitive = false,
+        IgnoreExtraArguments = false
+
+      });
+      commands.RegisterCommands<BotCommands>();
+      commands.SetHelpFormatter<CustomHelpFormatter>();
+
+      Client.UseInteractivity(new InteractivityConfiguration()
+      {
+        PollBehaviour = PollBehaviour.KeepEmojis,
+        Timeout = TimeSpan.FromSeconds(30),
+        ResponseBehavior = InteractionResponseBehavior.Ignore
+      });
+
+      await Client.ConnectAsync( new DiscordActivity("anything you say!", ActivityType.ListeningTo) , UserStatus.Online);
+      await Task.Delay(-1);
+    }
+    private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+    {
+      // let's log the fact that this event occured
+      //sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+      // since this method is not async, let's return
+      // a completed task, so that no additional work
+      // is done
+      return Task.CompletedTask;
+    }
+    private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+    {
+      // let's log the name of the guild that was just
+      // sent to our client
+      //sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+      // since this method is not async, let's return
+      // a completed task, so that no additional work
+      // is done
+      return Task.CompletedTask;
+    }
+    private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+    {
+      // let's log the details of the error that just 
+      // occured in our client
+      //sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+
+      // since this method is not async, let's return
+      // a completed task, so that no additional work
+      // is done
+      return Task.CompletedTask;
     }
   }
 }
---
+++ DiscordBotWPF/Server.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordBotWPF
+{
+  public class Server : INotifyPropertyChanged
+  {
+    //
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      try
+      {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    private int? myProperty;
+    public int? MyProperty
+    {
+      get { return myProperty; }
+      private set
+      {
+        myProperty = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public async Task InitializeAsync()
+    {
+      await Task.Delay(100);
+      MyProperty = 13;
+    }
+
+
+    private static HttpListener listener;
+    static Boolean running;
+    public Boolean Running
+    {
+      get { return running; }
+      set
+      {
+        if (running != value)
+        {
+          running = value;
+          OnPropertyChanged();
+        }
+      }
+    }
+    static string status;
+    public string Status
+    {
+      get { return status; }
+      set
+      {
+        if (status != value)
+        {
+          status = value;
+          OnPropertyChanged();
+        }
+      }
+    }
+
+
+    public void Start(string[] prefixes)
+    {
+      //PropertyChanged = new PropertyChangedEventHandler({ });
+      listener = new HttpListener();
+      foreach (string s in prefixes)
+      {
+        listener.Prefixes.Add(s);
+      }
+      try
+      {
+        listener.Start();
+        listener.BeginGetContext(OnContext, listener);
+        Status = "Waiting for requests";
+        Running = true;
+      }
+      catch (Exception ex)
+      {
+        Status = "Exception:" + ex.ToString();
+        Running = false;
+        //throw;
+      }
+      //Console.ReadLine();
+    }
+    private void OnContext(IAsyncResult ar)
+    {
+      var context = listener.EndGetContext(ar);
+      listener.BeginGetContext(OnContext, listener);
+      HttpListenerRequest request = context.Request;
+      HttpListenerResponse response = context.Response;// Obtain a response object.
+      // Construct a response.
+      string responseString = "<HTML><BODY>";
+      foreach (var cookieKey in request.QueryString.AllKeys)
+      {
+        responseString += "Key: " + cookieKey + ", Value:" + request.QueryString[cookieKey] + "<br/>";
+        if (cookieKey == "code")
+          //lock (new object())
+          //{
+          Status = request.QueryString[cookieKey];
+        //}
+        //Interlocked.
+      }
+      //NotifyPropertyChanged("Status");
+      responseString += "<a href='#' onclick='close();return false;'>close</a></BODY></HTML>";
+      byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+      // Get a response stream and write the response to it.
+      response.ContentLength64 = buffer.Length;
+      Stream output = response.OutputStream;
+      output.Write(buffer, 0, buffer.Length);
+      output.Close();
+    }
+
+  }
+}
